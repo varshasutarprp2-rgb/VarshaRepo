e5d03df449d81297557a88b30ff2a5a2601f22ee
varshasutarprp2-rgb <varsha.sutarprp2@gmail.com>
2026-07-17 16:48:49 +0530

    Create Course class with name and duration attributes

---
+++ Course.java
@@ -0,0 +1,18 @@
+public class Course {
+
+    private String courseName;
+    private int duration;
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
